4fd6d88760783ff17689e1facc10dde1d9f27122
Lyra Freund <lyrafreund@gmail.com>
2023-10-24 15:48:32 -0600

    completed lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,42 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
+    
+print(badge_maker("Erin"))
+
+names = ["Arel", "Carol"]
+
+
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
+    
+batches = [(f"Hello, my name is {name}.") for name in names]
+
+print(batches)
+print(batch_badge_creator(names))
+
+
 
 def assign_rooms(names):
-    return None
+    rooms = range(1, 3)
+    
+    assignments = []
+    for room in rooms:
+        assignments.append(f"Hello, {names[room -1]}! You'll be assigned to room {room}!")
+    
+    return assignments
+
+print(assign_rooms(names))
 
 def printer(names):
-    return None
+    for batch in batches:
+        print(batch)
+
+    assignments = assign_rooms(names)
+    for assignment in assignments:
+        print(assignment)
+
+print(printer(names))
